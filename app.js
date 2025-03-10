8f56c259dd5197cdcdec72e0007dec8391882984
etohimself <ertugrulcore@gmail.com>
2025-03-10 14:23:00 +0300

    Improved responsiveness, added JSON validation

--- app.js
+++ app.js
@@ -12,8 +12,69 @@ app.use(express.static(path.join(__dirname, "assets")));
 
 // Fetch data from JSON file
 function fetchJSON() {
-  let jsonFile = fs.readFileSync("assets/data/data.json", "utf8");
-  return JSON.parse(jsonFile);
+  let fetchedData = fs.readFileSync("assets/data/data.json", "utf8");
+  let parsedData = JSON.parse(fetchedData);
+  //Check if data is an array
+  if (!Array.isArray(parsedData)) {
+    throw new Error("Fetched JSON is not an array!");
+  }
+  //Iterate through all items, check for certain keys
+  let requiredKeys = [
+    "page",
+    "nav",
+    "route",
+    "title",
+    "description",
+    "events",
+    "resources",
+  ];
+  parsedData.forEach((eachItem) => {
+    requiredKeys.forEach((eachKey) => {
+      if (!Object.prototype.hasOwnProperty.call(eachItem, eachKey)) {
+        throw new Error(`${eachKey} is missing for a page data!`);
+      }
+    });
+  });
+
+  //Check if events and resources are both arrays for all items
+  parsedData.forEach((eachItem) => {
+    if (!Array.isArray(eachItem.events)) {
+      throw new Error("Fetched JSON's events key is not an array!");
+    }
+    if (!Array.isArray(eachItem.resources)) {
+      throw new Error("Fetched JSON's resources key is not an array!");
+    }
+  });
+
+  //Check each item of events key, look for date, text and image keys
+  requiredKeys = ["date", "text", "image"];
+  parsedData.forEach((eachPage) => {
+    eachPage.events.forEach((eachEvent) => {
+      requiredKeys.forEach((eachKey) => {
+        if (!Object.prototype.hasOwnProperty.call(eachEvent, eachKey)) {
+          throw new Error(
+            `${eachKey} is missing for an event in ${eachPage.page} page !`
+          );
+        }
+      });
+    });
+  });
+
+  //Check each resource for name and link
+  requiredKeys = ["name", "link"];
+  parsedData.forEach((eachPage) => {
+    eachPage.resources.forEach((eachResource) => {
+      requiredKeys.forEach((eachKey) => {
+        if (!Object.prototype.hasOwnProperty.call(eachResource, eachKey)) {
+          throw new Error(
+            `${eachKey} is missing for an resource in ${eachPage.page} page !`
+          );
+        }
+      });
+    });
+  });
+
+  return parsedData;
 }
 
 // Route for the homepage
@@ -21,7 +82,6 @@ app.get("/", (req, res) => {
   let data = fetchJSON();
   let pageData = data.find((item) => item.page === "index");
   let currentPage = "index";
-  console.log(data);
   res.render("layout", { pageData, data, currentPage });
 });
 
